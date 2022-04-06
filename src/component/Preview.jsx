3ce85b8bdd879d190936fac3dabc007e0f25a018
EverytimeAomsin <sarin542@hotmail.com>
2022-04-06 22:08:52 +0700

    แก้สแกน

--- src/component/Preview.jsx
+++ src/component/Preview.jsx
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBCardTitle, MDBCardImage, MDBCardText } from 'mdb-react-ui-kit';
 import ShowPrediction from "./ShowPrediction";
+import Swal from "sweetalert2"
 
 const Preview = ({ searchState }) => {
+  const [disabled, setDisabled] = useState("True");
   const [predictedData, setPredictedData] = useState({
     Flower: "",
     Probability: "0",
   });
 
   const handlePredict = function () {
-    if (searchState === "") {
+    if (searchState === "https://cdn.discordapp.com/attachments/556546837247426635/960659803980578896/img.jpg") {
       return;
     }
-
+    if (disabled === "False") {
+      Swal.fire('แจ้งเตือน', "มีรูปภาพอยู่แล้ว กดโอเคเพื่อรีหน้าแล้วใส่รูปใหม่", 'error').then((result) => {
+        // Reload the Page
+        window.location.reload();
+      });
+      return;
+    }
+    setDisabled("False");
     console.log(searchState);
     const data = {
       ImageUrl: searchState,
@@ -45,6 +54,9 @@ const Preview = ({ searchState }) => {
       }
     );
   };
+  function refreshPage() {
+    window.location.reload(false);
+  }
   return (
     <>
       <div className='search-img'>
@@ -76,7 +88,8 @@ const Preview = ({ searchState }) => {
         </MDBRow>
       </div>
       <div className='fixed-bottom  end-0 text-end'>
-        <button className='prediction-button' onClick={handlePredict}>สแกนอีกครั้ง</button>
+        <button className='prediction-button' onClick={handlePredict}>สแกนรูปภาพ</button>
+        <button className='refreshPage-button' onClick={refreshPage}>เปลียนรูปภาพ</button>
       </div>
     </>
   );
--- src/component/ShowPrediction.jsx
+++ src/component/ShowPrediction.jsx
@@ -1,12 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 const ShowPrediction = ({ predictedData }) => {
-  const Probability = predictedData.Probability ;
-  let ProbabilityFormatted=parseFloat(Probability).toFixed(2);
+  const [detail, setDetail] = useState("");
+  const [flower, setFlower] = useState("");
+  const [bloglink, setBloglink] = useState("");
+  const [check, setcheck] = useState("T");
+
+  const Probability = predictedData.Probability;
+  if (predictedData.Flower === "ดอกทานตะวัน ..." && check === "T") {
+    // setBlog("/intro/blog/(sunflower)")
+    setDetail("รายละเอียดดอกทานตะวัน");
+    setBloglink("/intro/blog/(sunflower)")
+    setFlower("ดอกทานตะวัน")
+    setcheck("F")
+  }
+  if (predictedData.Flower === "ดอกกุหลาบ (r..." && check === "T") {
+    setBloglink("/intro/blog/(sunflower)")
+    setcheck("F")
+  }
+  
+  let ProbabilityFormatted = parseFloat(Probability).toFixed(2);
   return (
     <>
       <h4 className='Kanit-regular text-center'>ผลลัพธ์</h4>
-      <h4 className='Kanit-regular'>ชนิดดอกไม้ : <span className="lightfont text-decoration-underline">{predictedData.Flower}</span></h4>
+      <h4 className='Kanit-regular'>ชนิดดอกไม้ : <span className="lightfont text-decoration-underline">{flower}</span></h4>
       <h4 className='Kanit-regular'>ความแม่นยำ : <span className="lightfont text-decoration-underline">{ProbabilityFormatted * 100} %</span></h4>
+      <h4 className='Kanit-regular'>บทความที่เกี่ยวข้อง : <span className="lightfont text-decoration-underline"><Link to={bloglink} >{detail} </Link></span></h4>
     </>
   );
 };
